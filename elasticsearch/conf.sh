e539da0e4c67afcd62e2a230979d395397a82058
root <root@srv-poclog-01.gnc.testrecif.nc>
2015-11-09 13:54:29 +1100

    Add proxy

--- elasticsearch/conf.sh
+++ elasticsearch/conf.sh
@@ -9,14 +9,14 @@ ES_NETWORK_HOST=${ES_NETWORK_HOST:-127.0.0.1}
 ES_NODE_NAME=${ES_NODE_NAME:-graylog-server}
 ES_MULTICAST_ENABLED=${ES_MULTICAST_ENABLED:-false}
 
-echo "lolES_CLUSTER_UNICAST_HOSTS=${ES_CLUSTER_UNICAST_HOSTS}"
+echo "ES_CLUSTER_UNICAST_HOSTS=${ES_CLUSTER_UNICAST_HOSTS}"
 echo "ES_CLUSTER_NAME=${ES_CLUSTER_NAME}"
 echo "ES_NETWORK_HOST=${ES_NETWORK_HOST}"
 echo "ES_NODE_NAME=${ES_NODE_NAME}"
 echo "ES_MULTICAST_ENABLED=${ES_MULTICAST_ENABLED}"
 
 echo "Installing elasticsearch-head plugin... "
-/usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head
+/usr/share/elasticsearch/bin/plugin -install -DproxyHost=192.168.100.1 -DproxyPort=80 mobz/elasticsearch-head
 
 echo "Starting Elasticsearch with configured parameters..."
 elasticsearch --node.name=${ES_NODE_NAME} --cluster.name=${ES_CLUSTER_NAME} --network.publish_host=${ES_NETWORK_HOST} --discovery.zen.ping.multicast.enabled=${ES_MULTICAST_ENABLED} --discovery.zen.ping.unicast.hosts=${ES_CLUSTER_UNICAST_HOSTS}
